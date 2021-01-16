67898a61301bb56df659e86249a0de9196e13449
CarlosShu <70170956+CarlosShu@users.noreply.github.com>
2021-01-16 05:22:52 -0500

    Started Sausage Dog Activity

---
+++ examples/first_class_functions/js/script.js
@@ -0,0 +1,38 @@
+// if you have a lot parameters in a function.
+
+"use strict";
+
+// first part.
+
+// function add(a, b) {
+//   return a + b;
+// }
+//
+// let addingFunction = add;
+//
+// let result = addingFunction(1, 10);
+//
+// alert(`The result is ${result}!`);
+
+// second part.
+
+// function hello() {
+//   alert(`Hello!`);
+// }
+//
+// setTimeout(hello, 5000); // delay function by set milliseconds.
+
+// third part: anonymous functions.
+
+// let hello = function () {
+//   alert("Hello!");
+// };
+//
+// setTimeout(hello, 5000);
+
+// fourth part.
+
+// setTimeout(function () {
+//   alert(`Hello!`);
+// }, 5000);
+
